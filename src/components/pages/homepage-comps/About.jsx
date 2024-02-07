19fa932f2be02b4260bc2e29da102e51ef11c403
ChrisADS <crissisullca@gmail.com>
2024-02-06 22:16:45 -0500

    se add

---
+++ src/components/pages/homepage-comps/About.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+import { Link } from 'react-router-dom'
+import { Link as ScrollLink } from 'react-scroll';
+
+const About = () => {
+    return (
+        <section name="About" 
+        className='relative w-full md:h-screen text-white h-unset'>
+
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full text-lg'>
+
+                    <div className='pb-8'>
+                        <h2 className='text-4xl sm:text-5xl font-bold inline border-b-4 border-primary-color/40'>Acerca de mí</h2>
+                    </div>
+
+                    <p className="mb-4 py-6">Estudiante de la carrera de Ingeniera de Sistemas e Informática, busco formar parte de una empresa en la que
+                    pueda poner en práctica todos mis conocimientos, que me brinde la oportunidad de alcanzar todas mis metas
+                    trazadas, y que me ofrezca la oportunidad de crecer en el área laboral, personal e intelectual
+                    .</p>
+
+                    <Link to='/about-me' className='text-black font-semibold text-[16px] w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-t from-green-400 to-primary-color cursor-pointer mx-auto md:mx-0 self-end mt-8 hover:scale-110 duration-300'>
+                        See more
+                        <span className=''><MdOutlineKeyboardArrowRight size={25} className='ml-1' /></span>
+                    </Link>
+                
+            </div>
+
+            <ScrollLink to="Projects" smooth duration={500} className='absolute bottom-2 -left-full md:left-1/2 md:-translate-x-1/2 cursor-pointer hover:text-primary-color'>
+                <i className='bx bx-chevron-down text-7xl text-gray-400 animate-bounce font hover:text-primary-color'></i>
+            </ScrollLink>
+            
+        </section>
+        
+    )
+}
+
+export default About
